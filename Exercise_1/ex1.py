90c1f31771e2619c0b53d8cbcb852f1e9f78e57e
Paweł Cybulski <pawel.cybulski81@gmail.com>
2026-03-04 22:09:43 +0100

    first exercise

---
+++ Exercise_1/ex1.py
@@ -0,0 +1,7 @@
+print("Hello World!")
+print("Hello Again")
+print("I like typing this.")
+print("This if fun.")
+print("Yay,! Printing.")
+print("I'd rather you 'not'.")
+print('I "said" do not touch this.')
