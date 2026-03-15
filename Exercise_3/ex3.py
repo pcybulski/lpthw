af2e6e9417a2b0ee4b662b0fc33e58f98bc1029f
Paweł Cybulski <pawel.cybulski81@gmail.com>
2026-03-15 20:55:16 +0100

    exercise 3

---
+++ Exercise_3/ex3.py
@@ -0,0 +1,31 @@
+# Just printing a phrase
+print("I will now count my chickens:")
+
+# Printing phrase, and doing devision and addition
+print("Hens", 25 + 30 /6)
+
+#Mulpiply 25 times 3 then divice by 4 and dispay the rest of division then do substraction.
+print("Roosters", 100-25 * 3 % 4)
+
+#Just a print
+print("Now I will count the eggs:")
+
+
+#Below there are just mathematical operations, and print true or false for equasions.
+print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
+
+print("Is it true that 3 + 2 < 5 - 7?")
+
+print(3 + 2 < 5 - 7)
+
+print("What is 3 + 2?", 3 + 2)
+
+print("Oh, that's why it's False.")
+
+print("How about some more.")
+
+print("It is greater?", 5 > -2)
+
+print("It is greater or equal?", 5 >= -2)
+
+print("Is it less or equal?", 5 <= -2 )
